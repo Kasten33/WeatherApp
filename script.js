ebcf510d0d131206fbe9a1b45c89c55b0f0eaa56
Kasten33 <hurlrykaden@gmail.com>
2023-10-25 17:44:49 -0600

    :skull:

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=tooele&appid=3af9b85e5e77699a72a01952e7b9b73b"
+const WURL = "https://api.openweathermap.org/data/2.5/weather?q=tooele&appid=3af9b85e5e77699a72a01952e7b9b73b"
 
-fetch(apiURL)
+fetch(WURL)
   .then((response) => response.json())
   .then((jsObject) => {
     // console.log(jsObject);
@@ -22,4 +22,41 @@ fetch(apiURL)
     const wc = 35.74 + 0.6215 * T - 35.75 * W ** 0.16 + 0.4275 * T * W ** 0.16;
     document.getElementById("current-windChill").textContent = Math.floor(wc);
     // console.log(wc);
+  });
+
+  const NURL = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-25&sortBy=publishedAt&apiKey=7925e6a5cce646bd99df2b1b16b7538c"
+
+  fetch(NURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+  document.getElementById("header-img").setAttribute('src', jsObject.articles[0].urlToImage) 
+  document.getElementById("header-article").textContent = jsObject.articles[0].title;
+  document.getElementById("header-desc").textContent = jsObject.articles[0].description;
+  document.getElementById("header-article").setAttribute('href', jsObject.articles[0].url) 
+
+  document.getElementById("article-img-2").setAttribute('src', jsObject.articles[1].urlToImage) 
+  document.getElementById("article-link-2").textContent = jsObject.articles[1].title;
+  document.getElementById("article-desc-2").textContent = jsObject.articles[1].description;
+  document.getElementById("article-link-2").setAttribute('href', jsObject.articles[1].url) 
+
+  document.getElementById("article-img-3").setAttribute('src', jsObject.articles[2].urlToImage) 
+  document.getElementById("article-link-3").textContent = jsObject.articles[2].title;
+  document.getElementById("article-desc-3").textContent = jsObject.articles[2].description;
+  document.getElementById("article-link-3").setAttribute('href', jsObject.articles[2].url) 
+
+  document.getElementById("article-img-4").setAttribute('src', jsObject.articles[3].urlToImage) 
+  document.getElementById("article-link-4").textContent = jsObject.articles[3].title;
+  document.getElementById("article-desc-4").textContent = jsObject.articles[3].description;
+  document.getElementById("article-link-4").setAttribute('href', jsObject.articles[3].url) 
+
+  document.getElementById("article-img-5").setAttribute('src', jsObject.articles[4].urlToImage) 
+  document.getElementById("article-link-5").textContent = jsObject.articles[4].title;
+  document.getElementById("article-desc-5").textContent = jsObject.articles[4].description;
+  document.getElementById("article-link-5").setAttribute('href', jsObject.articles[4].url) 
+
+
+
+
+
   });
